a1a75466cf16363e13f90e522d861230b27c664c
lukabugarin6 <lukabugarin6@gmail.com>
2026-01-13 16:01:32 +0100

    main ts update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://pizzaprojekat.com',
+      'https://www.pizzaprojekat.com',
+      'https://stage.pizzaprojekat.com',
+    ],
     credentials: true,
   });
 
