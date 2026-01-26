c0dab695adeec0ff293c55106bc223b74f0d39e6
lukabugarin6 <lukabugarin6@gmail.com>
2026-01-26 01:37:12 +0100

    analytics update

--- webfront/components/analytics.tsx
+++ webfront/components/analytics.tsx
@@ -1,23 +1,18 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function GoogleAnalytics() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (!GA_ID) return;
-
-    const query = searchParams?.toString();
-    const url = query ? `${pathname}?${query}` : pathname;
-
     // @ts-ignore
-    window.gtag?.('config', GA_ID, { page_path: url });
-  }, [pathname, searchParams]);
+    window.gtag?.('config', GA_ID, { page_path: pathname });
+  }, [pathname]);
 
   return null;
 }
